fd3bf82365c90e84e0dc8892095fd5487a4adba6
youpong <43817652+youpong@users.noreply.github.com>
2025-11-01 03:22:45 +0900

    Update startsignal.py

--- startsignal.py
+++ startsignal.py
@@ -1,62 +1,42 @@
 from microbit import *
 import time
 import utime
+import random
 
-def show_signal():
-    display.show(Image('00000:'
-                      '00000:'
-                      '00000:'
-                      '00000:'
-                      '00000:'))
-    sleep(1000)
-    display.show(Image('00000:'
-                      '00000:'
-                      '00000:'
-                      '90000:'
-                      '90000:'))
-    sleep(1000)
-    display.show(Image('00000:'
-                      '00000:'
-                      '00000:'
-                      '99000:'
-                      '99000:'))
-    sleep(1000)
-    display.show(Image('00000:'
-                      '00000:'
-                      '00000:'
-                      '99900:'
-                      '99900:'))
-    sleep(1000)
-    display.show(Image('00000:'
-                      '00000:'
-                      '00000:'
-                      '99990:'
-                      '99990:'))
-    sleep(1000)
-    display.show(Image('00000:'
-                      '00000:'
-                      '00000:'
-                      '99999:'
-                      '99999:'))
-    # TODO: randomize sleep time
-    sleep(1000)
-    display.show(Image('00000:'
-                      '00000:'
-                      '00000:'
-                      '00000:'
-                      '00000:'))    
+def wait_for(wait_ms):
+    wait_time = time.ticks_ms() + wait_ms
+    while wait_time > time.ticks_ms():
+        if button_a.is_pressed():
+            return False
+    return True
+
+def start_sequence():
+    # Light up first column
+    display.set_pixel(0, 3, 9)
+    display.set_pixel(0, 4, 9)
+
+    # Light up the subsequent column
+    for seq in range(1, 5):
+        if not wait_for(1000):
+            return False
+        display.set_pixel(seq, 3, 9)
+        display.set_pixel(seq, 4, 9)
+
+    # Turn off all columns
+    if not wait_for(random.randint(1000, 3000)):
+        return False
+    display.clear()
 
 while True:
-    display.scroll('Touch Logo to start')
-    while True:
-        if pin_logo.is_touched():
-            break
-        
-    show_signal()
+    if start_sequence() == False:
+        # display.scroll("False Start")
+        display.show(Image.NO)
+        break
+
     start_time = time.ticks_ms()
-    while True:
-        if button_a.is_pressed():
-            reacted_time = time.ticks_ms()
-            duration_time = utime.ticks_diff(reacted_time, start_time) / 1000
-            display.scroll(duration_time)
-            break
+    while not button_a.is_pressed():
+        sleep(1)
+    reaction_time = utime.ticks_diff(time.ticks_ms(), start_time)
+    display.scroll(reaction_time / 1000)
+    # display.scroll("RT: ()ms".format(reaction_time / 1000))
+#    display.show()
