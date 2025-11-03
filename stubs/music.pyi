514779595a829ff7b0fb24e85d5d90788b73f52f
NAKAJIMA Yusaku <yupong7@gmail.com>
2025-11-04 01:25:00 +0900

    wip

--- stubs/music.pyi
+++
@@ -1,7 +0,0 @@
-import microbit
-
-
-def pitch(frequency: int,
-          duration: int = -1,
-          pin: microbit.MicroBitAnalogDigitalPin = microbit.pin0,
-          wait: bool = True) -> None: ...
